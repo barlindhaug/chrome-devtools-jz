1a01e28bc9b9c940e89f7fc7b016ea845aa98464
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2016-09-05 19:18:04 +0200

    Add task for node debugging

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello you!!!!');
+  var numbers = ['10', '2', '0', '2'];
+  var numbersParsed = numbers.map(parseInt);
+  var sum = numbersParsed.reduce((a,b) => a + b);
+  res.send('Sum of the numbers: ' + sum);
 });
 
 app.listen(3000, function () {
